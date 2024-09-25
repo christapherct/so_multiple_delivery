8f127ad38bdc84850549e0cfe5aa6f47acd5cdb4
Christapher C T <christapherct02@gmail.com>
2024-09-25 20:32:37 +0530

    module has been initialized

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'SO Multiple DO',
+    'version': '17.0',
+    'category': 'Sales',
+    'summary': 'The module is designed to Customize the multiple delivery orders for each order lines',
+    'depends': ['sale_management', 'stock'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
